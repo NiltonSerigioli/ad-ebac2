846db26b77f586dbaa5fb3e2206f865688646120
NiltonSerigioli <nserigiolli@gmail.com>
2022-04-12 13:03:40 +0000

    altualização dos código e do gráfico

--- gasolina.py
+++ gasolina.py
@@ -3,9 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 venda_gasolina = pd.read_csv('gasolina.csv')
-
 with sns.axes_style('whitegrid'):
- grafico=sns.lineplot(data=venda_gasolina, x="dia", y="venda", color="red")
- grafico.set_title("Valor da Gasolina")
+ grafico=sns.lineplot(data=venda_gasolina, x="dia", y="venda", color="blue")
+ grafico.set_title("Variação de Preço da Gasolina na Semana")
  plt.show(grafico)
  plt.savefig('venda_gasolina.png')
